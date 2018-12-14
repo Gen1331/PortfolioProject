a7c0552afc2ec02e99dbe83534c4b9561fbe399f
likejean <popachs@yahoo.com>
2018-12-14 03:37:04 -0800

    added more images, empty folders and Week-2 Portfolio Project Report

--- scripts/imageReader.js
+++ scripts/imageReader.js
@@ -1,5 +1,6 @@
 
     
+var caption = '<div class="carousel-caption d-none d-md-block" style="vertical-align: baseline;"><h5 style="text-align: center;">Visit Georgia</h5>';
 
 
 
@@ -10,8 +11,8 @@ for(var i=0; i<arr.length; i++){
     img.src = "images/" + arr[i] + ".jpg";    
     var div = document.getElementById('imgRead');
     var ind = document.getElementById('indicator');   
-    i===0 ? div.innerHTML += '<div class="carousel-item active"><img style="width:100%" class="d-block w-100" src="' + img.src + '" alt="First slide"></div>' : 
-    div.innerHTML += '<div class="carousel-item"><img style="width:100%" class="d-block w-100" src="' + img.src + '" alt="First slide"></div>'; 
+    i===0 ? div.innerHTML += '<div class="carousel-item active"><img style="width:100%" class="d-block w-100" src="' + img.src + '" alt="First slide">' + caption + '</div>' : 
+    div.innerHTML += '<div class="carousel-item"><img style="width:100%" class="d-block w-100" src="' + img.src + '" alt="First slide">' + caption + '</div>'; 
     i===0 ? ind.innerHTML += '<li data-target="#carouselExampleIndicators" data-slide-to="' + i + '" class="active"></li>': 
     ind.innerHTML += '<li data-target="#carouselExampleIndicators" data-slide-to="' + i +'"></li>'; 
    
