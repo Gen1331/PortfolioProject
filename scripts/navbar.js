cc1fd7d5a661d784a269f85c7d5688532dd893b3
likejean <popachs@yahoo.com>
2018-12-17 11:00:36 -0800

    fixed navbar bugs

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
     $('.menu').click(function(){
         $('ul').toggleClass('active'); 
+
     });
+    window.onresize = function() { 
+        if ($(window).width() < 900){
+            $('.my-row').removeClass('align-items-center').addClass('align-items-start');  
+            $('.my-col').removeClass('col-3');
+            if($(window).width() < 400){
+                $('div.toggle').hide();
+                
+            }          
+        }else{
+            $('.my-row').removeClass('align-items-start').addClass('align-items-center');
+            $('.my-col').addClass('col-3');
+            $('ul').removeClass('active');
+            if ($('div.toggle').is(":hidden")){
+                $('div.toggle').show();
+            }
+        };
+    };
 });
