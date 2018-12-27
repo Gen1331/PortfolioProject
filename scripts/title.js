70a73834a60b3109efdc4edece642522ddf102f1
Gen1331 <Gen1331@gmail.com>
2018-12-26 19:12:54 -0800

    modified footer, js title, and request modal. added to activites page

--- scripts/title.js
+++ scripts/title.js
@@ -6,15 +6,15 @@ $(document).ready(function(){
     //Initialization: get the image id, carousel object and display the title of the first image
     var id = $(this).find("div").find(".active").find('img').attr('id');
     var crsl = $(this);
-    $('#title').html("~Welcome to " + title[0] + "~"); 
+    $('#title').html("~Welcome to Georgia: " + title[0] + "~"); 
     
     //Run the carousel automatically...
     $('#mycarousel').on('slide.bs.carousel', function (){                     
         id = crsl.find("div").find(".active").find('img').attr('id');            
         if(id == title.length){
-            $('#title').html("~Welcome to " + title[0] + "~");           
+            $('#title').html("~Welcome to Georgia: " + title[0] + "~");           
         }else{
-            $('#title').html("~Welcome to " + title[id] + "~");            
+            $('#title').html("~Welcome to Georgia: " + title[id] + "~");            
         }
     });  
 
@@ -24,10 +24,10 @@ $(document).ready(function(){
         id = crsl.find("div").find(".active").find('img').attr('id');
         if (id == 1){
             id = title.length;            
-            $('#title').html("~Welcome to " + title[title.length-1] + "~");           
+            $('#title').html("~Welcome to Georgia: " + title[title.length-1] + "~");           
         }else{
             id--;            
-            $('#title').html("~Welcome to " + title[id-1] + "~"); 
+            $('#title').html("~Welcome to Georgia: " + title[id-1] + "~"); 
         }         
     }); 
     
@@ -37,10 +37,10 @@ $(document).ready(function(){
         id = crsl.find("div").find(".active").find('img').attr('id');
         if (id == title.length){
             id = 1;            
-            $('#title').html("~Welcome to " + title[0] + "~");           
+            $('#title').html("~Welcome to Georgia: " + title[0] + "~");           
         }else{
             id++;            
-            $('#title').html("~Welcome to " + title[id-1] + "~"); 
+            $('#title').html("~Welcome to Georgia: " + title[id-1] + "~"); 
         }                  
     });  
     
