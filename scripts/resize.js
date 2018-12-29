87d87d0980b57b1276d64ab3ce1456de5983c9b1
likejean <popachs@yahoo.com>
2018-12-29 13:03:22 -0800

    css files cleaned

--- scripts/resize.js
+++ scripts/resize.js
@@ -1,9 +1,10 @@
 (function($) { 
-    $('.navbar-nav li a').css('font-size',$(window).innerWidth()/42 + 'px');
+    //Initialization
+    $('.navbar-nav li a, .navbar-text a').css('font-size',$(window).innerWidth()/42 + 'px');
     $('.carousel').css('height', $(window).width()/1.4 + 'px');
     $('.map').css('width', ($(window).width() - 20) + 'px');    
     $(window).resize(function(){       
-        $('.navbar-nav li a').css('font-size',$(window).innerWidth()/42 + 'px');
+        $('.navbar-nav li a, .navbar-text a').css('font-size',$(window).innerWidth()/42 + 'px');
         $('.carousel').css('height', $(window).width()/1.4 + 'px'); 
         $('.map').css('width', ($(window).width() - 20) + 'px');        
     }); 
