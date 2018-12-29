1b0cab7b269b05f8c55906b5128684bb26794713
Gen1331 <Gen1331@gmail.com>
2018-12-29 12:52:35 -0800

    ready for submission

--- scripts/resize.js
+++ scripts/resize.js
@@ -1,9 +1,9 @@
 (function($) { 
-    $('.navbar-nav li a').css('font-size',$(window).innerWidth()/42 + 'px');
+    $('.navbar-nav li a, navbar-text a').css('font-size',$(window).innerWidth()/42 + 'px');
     $('.carousel').css('height', $(window).width()/1.4 + 'px');
     $('.map').css('width', ($(window).width() - 20) + 'px');    
     $(window).resize(function(){       
-        $('.navbar-nav li a').css('font-size',$(window).innerWidth()/42 + 'px');
+        $('.navbar-nav li a, navbar-text a').css('font-size',$(window).innerWidth()/42 + 'px');
         $('.carousel').css('height', $(window).width()/1.4 + 'px'); 
         $('.map').css('width', ($(window).width() - 20) + 'px');        
     }); 
